b16acb33a1daf44148041c3ca15eb7cf239893cd
pkoooo <gabrielpius0311@gmail.com>
2026-01-15 23:22:35 +0300

    gradients

---
+++ components/ui/about-lamp.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { LampContainer } from "@/components/ui/lamp";
+
+export function AboutLamp() {
+  return (
+    <LampContainer className="min-h-[300px] md:min-h-[400px]">
+      <div className="h-full" />
+    </LampContainer>
+  );
+}
